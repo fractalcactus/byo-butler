0bb29a6d69f56f97aef5d508c11ab917e9c1cd50
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-14 17:55:24 +1200

    attempt to interpolate date

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,3 +1,3 @@
 DATE= date +"%d-%b-%Y"    
 curl --request POST \
-  --url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=${DATE}&idBoard=$BOARD_ID&pos=2.0"
+  --url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d-%b-%Y' to sunday)&idBoard=$BOARD_ID&pos=2.0"
