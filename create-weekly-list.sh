e98295c4772290f2c1014d4da1829785d83781cb
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-14 17:41:47 +1200

    correct env name

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,2 +1,2 @@
     curl --request POST \
-  --url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=imfromherokusbashscript&idBoard=BOARD_ID&pos=2.0"
+  --url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=imfromherokusbashscript&idBoard=$BOARD_ID&pos=2.0"
