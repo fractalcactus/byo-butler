05ab20fde26749d9068a3394fd580d5671e3e95b
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-14 17:48:57 +1200

    attempt to interperolate date in list name

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,2 +1,3 @@
-    curl --request POST \
-  --url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=imfromherokusbashscript&idBoard=$BOARD_ID&pos=2.0"
+DATE= date +"%d-%b-%Y"    
+curl --request POST \
+  --url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=${DATE}&idBoard=$BOARD_ID&pos=2.0"
