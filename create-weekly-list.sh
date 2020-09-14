8dfeb9d6a6772b4441c3e18c54677bac56fc3c2c
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-14 18:29:03 +1200

    encode whitespace

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,2 +1,2 @@
 curl --request POST \
-	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d %b') to $(date +'%d %b' -d '+7 days') $(date +'%Y')&idBoard=$BOARD_ID&pos=2.0"
+	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d %b')%20to%20$(date +'%d %b' -d '+7 days')%20$(date +'%Y')&idBoard=$BOARD_ID&pos=2.0"
