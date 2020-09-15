70732957bca771e54c020dce76de1becec57d7e7
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-15 12:52:55 +1200

    escape percentage sign in date interpolation

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,2 +1,2 @@
 curl --request POST \
-	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d%20%b')%20to%20$(date +'%d%20%b' -d '+7 days')%20$(date +'%Y')&idBoard=$BOARD_ID&pos=2.0"
+	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d%%20%b')%20to%20$(date +'%d%%20%b' -d '+7 days')%20$(date +'%Y')&idBoard=$BOARD_ID&pos=2.0"
