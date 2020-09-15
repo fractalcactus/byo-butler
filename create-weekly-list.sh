51c9a92bbfc107f12668b8e4ee17cbbfc9e648a7
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-15 15:35:21 +1200

    correct date

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,3 +1,3 @@
-nz_hour="$(TZ=NZ date +%H)" ; nz_day="$(TZ=NZ date +%a)" ; if [ "$nz_hour" = 01 ] && [ "$nz_day" = "Mon" ]; then
+nz_day="$(TZ=NZ date +%a)" ;if [ "$nz_day" = "Mon" ]; then
 curl --request POST \
 	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d%%20%b')%20to%20$(date +'%d%%20%b' -d '+7 days')%20$(date +'%Y')&idBoard=$BOARD_ID&pos=2.0" ; fi
