6fb4ed69e0100708209aa1470518bd0a98b403e7
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-15 16:24:39 +1200

    add seedling emoji to meditation script

--- create-daily-meditation-card.sh
+++ create-daily-meditation-card.sh
@@ -1,2 +1,2 @@
 curl --request POST \
-	--url "https://api.trello.com/1/cards?key=$KEY&token=$TOKEN&idList=$TODAY_LIST_ID&name=commitment%20to%20values%20-%20meditate%20$(date +'%d%%20%b')%20🌱"
+	--url "https://api.trello.com/1/cards?key=$KEY&token=$TOKEN&idList=$TODAY_LIST_ID&name=commitment%20to%20values%20-%20meditate%20$(date +'%d%%20%b')%20%F0%9F%8C%B1"
