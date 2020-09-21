70dcb1237f2c19a69fa0e8dc492b4afe0375f61d
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2020-09-21 13:10:28 +1200

    correct weekly list start date to a day forward

--- create-weekly-list.sh
+++ create-weekly-list.sh
@@ -1,3 +1,3 @@
 nz_day="$(TZ=NZ date +%a)" ;if [ "$nz_day" = "Mon" ]; then
 curl --request POST \
-	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d%%20%b')%20to%20$(date +'%d%%20%b' -d '+7 days')%20$(date +'%Y')&idBoard=$BOARD_ID&pos=3.0" ; fi
+	--url "https://api.trello.com/1/lists?key=$KEY&token=$TOKEN&name=$(date +'%d%%20%b' -d '+1 days')%20to%20$(date +'%d%%20%b' -d '+7 days')%20$(date +'%Y')&idBoard=$BOARD_ID&pos=3.0" ; fi
